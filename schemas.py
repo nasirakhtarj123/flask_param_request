d7afd9481eb950f947706db5b800598430721ddd
Nasir Akhtar <nasirakhtarj@gmail.com>
2024-02-27 18:56:45 +0530

    initial commit

--- schemas.py
+++ schemas.py
@@ -1,6 +1,12 @@
 from marshmallow import Schema, fields
 
-class PersonSchema(Schema):
+class ParentSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    age = fields.Int(required=True)
+
+
+class ParentsSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     age = fields.Int(required=True)
