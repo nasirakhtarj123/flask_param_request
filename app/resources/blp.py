e6fb1d3ef13a48ac84d54744897c8cafc84bb5d9
Nasir Akhtar <nasirakhtarj@gmail.com>
2024-02-28 18:42:15 +0530

    initial commit

--- app/resources/blp.py
+++ app/resources/blp.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask.views import MethodView
 from app.models.persons import Persons
 from app import db
@@ -9,46 +9,68 @@ blp = Blueprint('api', __name__)
 
 @blp.route('/humans')
 class PeopleAPI(MethodView):
-    @blp.response(200, ParentsSchema)
-    def get(self):
-        persons = Persons.query.all()
-        # Serialize the data using the schema
-        serialized_data = ParentsSchema(many=True).dump(persons)
-        return jsonify(serialized_data)
 
-    @blp.arguments(ParentSchema)
+    @blp.response(200, ParentsSchema(many=True))
+    def get(self):
+        age = request.args.get('age')
+        name = request.args.get('name')
+        person_id = request.args.get('id')
+        query_filter = {}
+        if age:
+            query_filter['age'] = age
+        if name:
+            query_filter['name'] = name
+        if person_id:
+            query_filter['id'] = person_id
+        persons = Persons.query.filter_by(**query_filter).all()
+        result = [{'id': person.id, 'name': person.name, 'age': person.age} for person in persons]
+        return jsonify(result)
+
     @blp.response(201, ParentSchema)
-    def post(self, data):
-        new_person = Persons(name=data['name'], age=data['age'])
+    def post(self):
+        name = request.args.get('name')
+        age = request.args.get('age')
+        if not name or not age:
+            return jsonify({"message": "Name and age are required parameters"}), 400
+        new_person = Persons(name=name, age=age)
         db.session.add(new_person)
         db.session.commit()
-        return new_person, 201
+        result = {'id': new_person.id, 'name': new_person.name, 'age': new_person.age}
+        return jsonify(result), 201
 
-@blp.route('/humans/<int:person_id>')
+@blp.route('/humans', methods=['PUT', 'DELETE'])
 class PersonAPI(MethodView):
-    @blp.response(200, ParentSchema)
-    def get(self, person_id):
-        person = Persons.query.get(person_id)
-        if person:
-            return person
-        return {"message": "person not found"}, 404
 
-    @blp.arguments(ParentSchema)
     @blp.response(200, ParentSchema)
-    def put(self, data, person_id):
-        person = Persons.query.get(person_id)
-        if person:
-            person.name = data['name']
-            person.age = data['age']
+    def put(self):
+        try:
+            person_id = request.args.get('id')
+            name = request.args.get('name')
+            age = request.args.get('age')
+            if person_id is None:
+                return jsonify({"message": "Person ID is required"}), 400
+            person = Persons.query.get_or_404(person_id)
+            if name:
+                person.name = name
+            if age:
+                person.age = age
             db.session.commit()
-            return person
-        return {"message": "person not found"}, 404
+            updated_person = {'id': person.id, 'name': person.name, 'age': person.age}
+            return jsonify(updated_person), 200
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"message": str(e)}), 500
 
-    @blp.response(200, ParentSchema)
-    def delete(self, person_id):
-        person = Persons.query.get(person_id)
-        if person:
+    @blp.response(200)
+    def delete(self):
+        try:
+            person_id = request.args.get('id')
+            if person_id is None:
+                return jsonify({"message": "Person ID is required"}), 400
+            person = Persons.query.get_or_404(person_id)
             db.session.delete(person)
             db.session.commit()
-            return {"message": "Person deleted successfully"}, 200
-        return {"message": "person not found"}, 404
+            return jsonify({"message": "Person deleted successfully"}), 200
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"message": str(e)}), 500
